0af018b8f3512c57bc496337c8a5d42ee96a4b16
Namrata Chandarana <namratachandarana20@gmail.com>
2024-06-11 01:39:46 +0530

    modification

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,25 +7,55 @@ import 'react-toastify/dist/ReactToastify.css';
 import Blogs  from './pages/Blogs';
 import Blog  from './pages/Blog';
 import Publish from './pages/Publish';
-
+import AuthLayout from './component/AuthLayout';
+import Profile from './pages/Profile';
+import EditPost from './pages/EditPost';
+import Home from "./pages/Home";
+import { Toaster } from 'sonner'
 
 function App() {
-  // const [count, setCount] = useState(0)
-  
+
   return (
     <>
+    
       <Router>
         <Routes>
             <Route path='/signin' element={<Signin />}></Route>
             <Route path='/signup' element={<Signup />}></Route>
-            <Route path='/' element={<Blogs />}></Route>
-            <Route path='/blog/:id' element={<Blog />}></Route>
-            <Route path='/publish' element={<Publish />}></Route>
+            <Route path='/' element={<Home />}>
+            </Route>
+            <Route path='/blog' element={
+                <AuthLayout>
+                    <Blogs />
+                </AuthLayout>}>
+            </Route>
+            <Route path='/blog/:id' element={
+              <AuthLayout>
+                    <Blog />
+                </AuthLayout>}>
+            </Route>
+            <Route path='/publish' element={
+                <AuthLayout>
+                    <Publish />
+                </AuthLayout>}>
+            </Route>
+            <Route path='/profile' element={
+                <AuthLayout>
+                    <Profile />
+                </AuthLayout>}>
+            </Route>
+            <Route path='/edit/:id' element={
+                <AuthLayout>
+                    <EditPost />
+                </AuthLayout>}>
+            </Route>
         </Routes>
       </Router>
+      <Toaster />
       <ToastContainer />
     </>
   )
 }
 
-export default App
+
+export default App;
