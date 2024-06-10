2cecb630d0f68d3a6c1f52ff4d1e6e7aa5f496b0
Namrata Chandarana <namratachandarana20@gmail.com>
2024-06-11 01:16:13 +0530

    Appbar modified

--- frontend/src/component/Appbar.tsx
+++ frontend/src/component/Appbar.tsx
@@ -1,20 +1,39 @@
-import Avtar from "./Avtar";
-import { Link } from "react-router-dom";
+import { Link} from "react-router-dom";
+import 'react-toastify/dist/ReactToastify.css';
+import Dropdown from "./Dropdown";
 
+const Appbar = () => {
+    
+    const authStatus = localStorage.getItem("token");    
 
-const Appbar = ({name}: {name: string}) => {
         return (
             <>  
-                <div className="flex justify-between px-10 py-2 h-15 w-full border-b border-gray-200 sticky top-0 bg-white z-[100]">
-                    <div className="font-bold text-xl">Medium</div>
-                    <div>
-                    <Link to="/publish" className="bg-green-500 hover:bg-green-600 text-white font-bold py-1 px-4 rounded-full mx-2">
-                        Publish
-                    </Link>
-                        <Avtar name={name} size={"w-9 h-9"}/>
+                <div className="flex justify-between px-5 md:px-8 py-2 h-16 w-full border-b border-gray-200 sticky top-0 bg-white z-[100]">
+                    <div className="font-bold text-xl my-1">
+                        <Link to={authStatus? "/blog" : "/"}>DevTales</Link>
                     </div>
+                    {
+                        authStatus ? 
+                            <div>
+                                <Link to="/publish" className="font-georgian bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-5 rounded-full mx-2">
+                                    Publish
+                                </Link>
+                                <Dropdown />
+                            </div>
+                            :
+                            <div className="my-1 ">
+                                <button className="font-georgian bg-black hover:bg-black text-white font-semibold py-2 px-1 rounded-md mx-2 md:px-6 md:py-3 text-sm md:text-md">
+                                    <Link to="/signup">Get Started 	&rarr;</Link> 
+                                </button>
+                                <Link to="/signin" className="font-georgian bg-gray-200 hover:bg-gray-200 text-black font-semibold py-2 md:px-6 md:py-3 px-2 rounded-md text-sm md:text-md">
+                                    <Link to="/signin">Sign in</Link>
+                                
+                                </Link>
+                            </div>
+                    }
+                    
+                    
                 </div>
-                
             </>
         )
 }
--- frontend/src/component/Avtar.tsx
+++ frontend/src/component/Avtar.tsx
@@ -1,13 +1,18 @@
 
 
-const Avtart = ({name, size}: {name: string, size: string}) => {
+const Avtar = ({name, size}: {name: string,size: string}) => {
+
     return(
-        <div className={`relative inline-flex items-center ${size} justify-center overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600 cursor-pointer`}>
-            <span className="font-medium text-gray-600 dark:text-gray-300">
-                {name.split(" ").map(word => word.charAt(0).toUpperCase()).join("")}
-            </span>
-        </div>
+        <>
+            <div className={`relative inline-flex items-center ${size} justify-center overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600 cursor-pointer ` } id="dropdownUserAvatarButton" data-dropdown-toggle="dropdownAvatar">
+                <span className="font-medium text-gray-600 dark:text-gray-300">
+                    {name.split(" ").map(word => word.charAt(0).toUpperCase()).join("")}
+                </span>
+            </div>
+        </>
+       
+        
     )
 }
 
-export default Avtart
+export default Avtar
