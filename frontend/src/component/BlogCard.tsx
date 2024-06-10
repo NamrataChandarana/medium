5931b9923f5136c2c84c3eb86e6d824c519aba02
Namrata Chandarana <namratachandarana20@gmail.com>
2024-06-11 00:49:32 +0530

    All blogs page modified

--- frontend/src/component/BlogCard.tsx
+++ frontend/src/component/BlogCard.tsx
@@ -4,9 +4,10 @@ import Avtar from "./Avtar"
 interface BlogCardPorps  {
     authorName: string,
     title: string,
-    content: string,
-    publishedDate: string,
-    id: string
+    content: string  | React.JSX.Element | React.JSX.Element[],
+    publishedDate: Date,
+    id: string,
+    readTime: number
 }
 
 const BlogCard= ({
@@ -14,25 +15,26 @@ const BlogCard= ({
     title,
     content,
     publishedDate,
-    id
+    id,
+    readTime
 }: BlogCardPorps) => {
     
-
+const time = new Date(publishedDate);
     return (
         <>  
         <Link to={`/blog/${id}`}>
-            <div className=" mx-10 min-w-[38rem] max-w-[38rem]">
+            <div className=" lg:min-w-[38rem] max-w-[38rem]">
                 <div className="flex flex-col space-y-2 p-6 border-b border-gray-200 ">
                     <div className="flex items-center space-x-1">
                         <Avtar name={authorName} size={"w-9 h-9"}/>
                         <span className="font-semibold text-gray-800">{authorName}</span>
-                        <span className="text-sm text-gray-500 pt-1 ">&#8226; {publishedDate}</span>
+                        <span className="text-sm text-gray-500 pt-1 ">&#8226; {time.getDate()}-{time.getMonth()}-{time.getFullYear()} </span>
                     </div>
                     <div className="text-xl font-bold text-gray-800">{title}</div>
                     <div className="text-gray-700 text-wrap overflow-hidden text-justify">
-                        {content.length > 100 ? `${content.substring(0, 145)}...` : content}
+                    {content}   
                     </div>
-                    <div className="text-gray-500 text-sm pt-5">{`${Math.ceil(content.length / 100 )} min read`}</div>
+                    <div className="text-gray-500 text-sm pt-5">{`${readTime} min read`}</div>
                 </div>
             <div></div>
             </div>
--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -1,84 +1,89 @@
 import BlogCard from "../component/BlogCard"
 import axios from 'axios'
 import { useEffect, useState} from 'react';
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { BACKEND_URL } from "../config";
-import Appbar from "../component/Appbar";
+import {  toast } from 'sonner'
 import Skeleton from "../component/BlogsSkeleton";
+import { truncate } from "../component/TruncateText";
+import { useDispatch } from "react-redux"; 
+import { postsSuccess } from "../redux/reducer/postsSlice";
+import Appbar from "../component/Appbar";
 
+export interface Post{
+  "content": string;
+  "title": string;
+  "id": string;
+  "publishedDate": Date;
+  "author": {
+      "name": string;
+  };
+}
 
-// interface Blog {
-//   "content": string;
-//   "title": string;
-//   "id": string; 
-//   "author": {
-//       "name": string
-//   }
-// }
 const Blogs= () => {
 
-    const [posts, setPosts] = useState([]);
     const [loading, setLoading] = useState(true);
-    
-    async function getBlogs() {
-        try{
-          const res = await axios.get(`${BACKEND_URL}/api/v1/post/bulk`,{
-            headers: {
-              Authorization: localStorage.getItem("token")
-            }
-          });
+    const dispatch = useDispatch();
+    const [posts, setPosts] = useState<Post[]>();
+    const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
-          if(res.data.posts){  //check when no any published post on db
-            setPosts(res.data.posts);
-            setLoading(false)
+    async function getBlogs () {
+      try{
+        const res = await axios.get(`${BACKEND_URL}/api/v1/post/bulk`,{
+          headers: {
+            Authorization: localStorage.getItem("token")
           }
-          
-        }catch(err){
-          toast.error("Something Went wrong", {
-            position: "top-center",
-            autoClose: 5000,
-            hideProgressBar: true,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-            theme: "light",
-            });
+        });
+    
+        if(res.data.posts){  //check when no any published post on db
+          dispatch(postsSuccess(res.data.posts))
+          setPosts(res.data.posts)
+          setLoading(false)
         }
+        
+      }catch(err){
+        toast.error("Something Went wrong", {position: "top-center"});
       }
+    }
+    
+    function readTime(content: string){
+      const wordsPerMinute = 250;
+      const words = content.split('').length;
+      const readingTimeMinutes = words / wordsPerMinute;
+      const time = Math.ceil(readingTimeMinutes )
+      return time
+    }
 
-      useEffect(()=>{
-        getBlogs();
-      },[])
-
-      if(loading){
-        return(
-          <div>
-            <Appbar name="Namu" />
-            <Skeleton />
-            <Skeleton />
-            <Skeleton />
-            {/* <Skeleton /> */}
-            {/* <Skeleton /> */}
-          </div>
-        )
-      }
-
+    useEffect(()=>{
+      getBlogs();
+    },[dispatch])
+      
+    if(loading){
+      return(
+        <div>
+          <Appbar/>
+          <Skeleton />
+          <Skeleton />
+          <Skeleton />
+        </div>
+      )
+    }
+    
+    
     return (
       <>
-        <Appbar name="Namu" />
-         {posts.map((post)=>(
+        <Appbar />
+         {posts && posts.map(post=>(
           <div className="flex place-content-center">
             <BlogCard
-            authorName = {post.author.name || "Anonymous"}
-            title = {post.title}   
-            content = {post.content}
-            publishedDate = {"2nd nov"}
-            id={post.id}
+              authorName = {post.author.name || "Anonymous"}
+              title = {post.title}   
+              content = {truncate(post.content)}
+              publishedDate = {post.publishedDate}
+              id={post.id}
+              readTime={readTime(post.content)}
             />
           </div>  
         ))}
+         
       </>
     )
 }
