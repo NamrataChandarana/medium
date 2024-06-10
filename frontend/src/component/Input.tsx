58aefa93ba33ed8a7679ba834fb91a08c1697b01
Namrata Chandarana <namratachandarana20@gmail.com>
2024-06-11 00:36:28 +0530

    auth feature

--- frontend/src/component/Input.tsx
+++ frontend/src/component/Input.tsx
@@ -9,9 +9,9 @@ interface InputProps {
   }
  const Input: React.FC<InputProps> = ({ label, placeholder, type, id, onChange, value }) =>  {
     return(
-        <div className="space-y-2">
+        <div className="space-y-2 px-8 ">
             <label htmlFor={id}>{label}</label><br />
-            <input className="p-[.4rem] w-full border border-gray-200 rounded-md" value={value} id={id} placeholder={placeholder} type={type} onChange={onChange} required />
+            <input className="p-[.4rem] w-full  rounded-md" style={{ borderColor: "black"}} value={value} id={id} placeholder={placeholder} type={type} onChange={onChange} required />
         </div>
     )
 }
