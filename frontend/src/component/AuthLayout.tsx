295d534df130b381ca77c484a17b897a7a2c000f
Namrata Chandarana <namratachandarana20@gmail.com>
2024-06-11 01:38:50 +0530

    AuthLayout added

---
+++ frontend/src/component/AuthLayout.tsx
@@ -0,0 +1,26 @@
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect, ReactNode } from "react";
+
+
+interface AuthLayoutProps {
+    children: ReactNode;
+}
+
+const AuthLayout = ({children}: AuthLayoutProps) => {
+   const navigate = useNavigate();
+   const [loader, setLoader] = useState(true)
+   const authStatus = localStorage.getItem("token");
+   console.log(authStatus)
+
+   useEffect(()=>{
+    if(!authStatus){
+        navigate("/signin");
+        return;
+    }
+    setLoader(false)
+    },[navigate, authStatus]) 
+
+    return loader ? <h1>Loading...</h1> : <>{children}</>
+}
+
+export default AuthLayout;
